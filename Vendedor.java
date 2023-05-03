a351f78855edf23be694bf5f2b0bd0eaac1b3129
EloyTejero <tejero.eloyet36@gmail.com>
2023-05-03 10:11:10 -0300

    first commit, Class Vendedor (falta terminar el toString())

---
+++ Vendedor.java
@@ -0,0 +1,15 @@
+package TpPOO_03;
+
+public class Vendedor extends Empleado{
+    private double valorVenta;
+    
+    public Vendedor(String n, byte e, Sueldo sueldo, double valorVenta){
+        super(n, e, sueldo);
+        this.valorVenta = valorVenta;
+    }
+    
+    public void comision(){
+        double comision =  (2*valorVenta)/100;
+        this.getSueldo().setSueldoNeto(this.getSueldo().getSueldoNeto()+comision);
+    }
+}
